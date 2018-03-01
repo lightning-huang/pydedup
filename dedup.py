8dc9928e12a06939d1374107771a9526af494323
轩辕隐天山 <16414556@qq.com>
2018-03-01 23:33:05 +0800

    initial script
    just very simple and useful, first 1MB and filesize to dedup.

---
+++ dedup.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+import os,sys,hashlib
+from os.path import join,getsize
+
+picked_size = 1024000
+
+if len(sys.argv) < 2:
+	print('USAGE: python dedup.py {directory}')
+	sys.exit(-1)
+
+def mark_gabage(l, file, seed):
+	l.append(file)
+	print('same as %s, gabage found: %s'%(seed,file))
+
+hash_repo = {}
+gabage = []
+for root, dirs, files in os.walk(sys.argv[1]):
+	if len(files) > 0:
+		for file in files:
+			full_path = join(root,file)
+			file_size = getsize(full_path)
+			if file_size <= 0:
+				mark_gabage(gabage, full_path)
+				continue
+			f = open(full_path,'rb')
+			bytesfromfile = f.read(picked_size)
+			f.close()
+			current_md5 = hashlib.md5()
+			current_md5.update(bytesfromfile)
+			current_md5.update(('%s'%file_size).encode('UTF-8'))
+			current_md5_hex = current_md5.hexdigest()
+			if current_md5_hex in hash_repo.keys():
+				mark_gabage(gabage, full_path, hash_repo[current_md5_hex])
+			else:
+				hash_repo[current_md5_hex] = full_path
+				print('keep %s'%full_path)
+				
+for f in gabage:
+	command = 'del /F /Q \"%s\"'%f
+	print('kill dup, execute - ' + command)
+	os.system(command)
+				
